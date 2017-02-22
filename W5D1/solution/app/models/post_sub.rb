f6e5dc18c1794b7ad0f6a1d8ceabd630d506c559
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-02-22 00:39:23 -0800

    Initial commit W5D3 homework

--- W5D1/solution/app/models/post_sub.rb
+++
@@ -1,6 +0,0 @@
-class PostSub < ActiveRecord::Base
-  validates :post, :sub, presence: true
-
-  belongs_to :post
-  belongs_to :sub
-end
--- W5D1/solution/spec/models/user_vote_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe UserVote do
-
-  it { should validate_presence_of(:user) }
-
-  it { should belong_to(:user) }
-  it { should belong_to(:votable) }
-end
